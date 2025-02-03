45e04584219b785b9523a4cf18ccd7fa58a54d64
lucas.guerreiro <>
2025-02-02 23:28:15 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TetrisGame from './components/TetrisGame';
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-indigo-50 flex items-center justify-center">
+      <TetrisGame />
+    </div>
+  );
+}
+
+export default App;
